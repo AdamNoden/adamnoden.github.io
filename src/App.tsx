6e65e75338cbe8f8fdf4d12524cd5da46c05e4f1
adamnoden <107278274+adamnoden@users.noreply.github.com>
2022-06-30 19:22:49 +0100

    use hashrouter instead of browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { PageSwitch } from "./components";
 import "./index.css";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <PageSwitch />
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
